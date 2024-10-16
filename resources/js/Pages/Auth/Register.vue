e681904c726d6a5ceb5efef9d49f80236b46af36
Pei-Han Jiang <peihan945@gmail.com>
2024-10-16 09:49:21 +0800

    Finder Input done

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -25,21 +25,15 @@ const submit = () => {
 
 <template>
     <GuestLayout>
+
         <Head title="Register" />
 
         <form @submit.prevent="submit">
             <div>
                 <InputLabel for="name" value="Name" />
 
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
+                <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus
+                    autocomplete="name" />
 
                 <InputError class="mt-2" :message="form.errors.name" />
             </div>
@@ -47,14 +41,8 @@ const submit = () => {
             <div class="mt-4">
                 <InputLabel for="studentID" value="Student ID" />
 
-                <TextInput
-                    id="studentID"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.studentID"
-                    required
-                    autocomplete="username"
-                />
+                <TextInput id="studentID" type="text" class="mt-1 block w-full" v-model="form.studentID" required
+                    autocomplete="username" />
 
                 <InputError class="mt-2" :message="form.errors.studentID" />
             </div>
@@ -62,17 +50,12 @@ const submit = () => {
             <div class="mt-4">
                 <InputLabel for="major" value="Major" />
 
-                <select
-                    id="major"
-                    type="text"
+                <select id="major" type="text"
                     class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                    v-model="form.major"
-                    required
-                    autocomplete="username"
-                >
-                <option value="Computer Science">Computer Science</option>
-                <option value="Computer Science">Electrical Engineer</option>
-                <option value="Computer Science">Literature and History</option>
+                    v-model="form.major" required autocomplete="username">
+                    <option value="Computer Science">Computer Science</option>
+                    <option value="Electrical Engineer">Electrical Engineer</option>
+                    <option value="Literature and History">Literature and History</option>
 
                 </select>
 
@@ -82,18 +65,13 @@ const submit = () => {
             <div class="mt-4">
                 <InputLabel for="grade" value="Grade" />
 
-                <select
-                    id="grade"
-                    type="text"
+                <select id="grade" type="text"
                     class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                    v-model="form.grade"
-                    required
-                    autocomplete="username"
-                >
-                <option value="1">1st</option>
-                <option value="2">2nd</option>
-                <option value="3">3rd</option>
-                <option value="4">4th</option>
+                    v-model="form.grade" required autocomplete="username">
+                    <option value="1">1st</option>
+                    <option value="2">2nd</option>
+                    <option value="3">3rd</option>
+                    <option value="4">4th</option>
 
                 </select>
 
@@ -103,14 +81,8 @@ const submit = () => {
             <div class="mt-4">
                 <InputLabel for="email" value="Email" />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-                />
+                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required
+                    autocomplete="username" />
 
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
@@ -118,52 +90,28 @@ const submit = () => {
             <div class="mt-4">
                 <InputLabel for="password" value="Password" />
 
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-                />
+                <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
+                    autocomplete="new-password" />
 
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
             <div class="mt-4">
-                <InputLabel
-                    for="password_confirmation"
-                    value="Confirm Password"
-                />
+                <InputLabel for="password_confirmation" value="Confirm Password" />
 
-                <TextInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-                />
+                <TextInput id="password_confirmation" type="password" class="mt-1 block w-full"
+                    v-model="form.password_confirmation" required autocomplete="new-password" />
 
-                <InputError
-                    class="mt-2"
-                    :message="form.errors.password_confirmation"
-                />
+                <InputError class="mt-2" :message="form.errors.password_confirmation" />
             </div>
 
             <div class="mt-4 flex items-center justify-end">
-                <Link
-                    :href="route('login')"
-                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                    Already registered?
+                <Link :href="route('login')"
+                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                Already registered?
                 </Link>
 
-                <PrimaryButton
-                    class="ms-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
+                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Register
                 </PrimaryButton>
             </div>
--- resources/js/Pages/Finder.vue
+++ resources/js/Pages/Finder.vue
@@ -33,27 +33,100 @@ const form = useForm({
             <div class="py-12">
                 <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                        <!-- Finder Board Section -->
                         <div class="p-6 text-gray-900">
                             This is finder board
                         </div>
-                        <!-- Course ID Input -->
-                        <div class="mt-4">
-                            <InputLabel for="courseID" value="Course ID" />
 
-                            <TextInput 
-                                id="courseID" 
-                                type="text" 
-                                class="mt-1 block w-full" 
-                                placeholder="Course ID"
-                                v-model="form.courseID"
-                            />
-                            
-                            
+                        <!-- Form Section with aligned and spaced inputs -->
+                        <div class="p-6 text-gray-900">
+                            <div class="flex justify-start gap-x-8">
+                                <!-- Course ID Input -->
+                                <div>
+                                    <InputLabel for="courseID" value="Course ID" />
+                                    <TextInput id="courseID" type="text" class="mt-1 block w-full"
+                                        placeholder="Course ID" v-model="form.courseID" />
+                                </div>
+
+                                <!-- Course Title Input -->
+                                <div>
+                                    <InputLabel for="courseTitle" value="Course Title" />
+                                    <TextInput id="courseTitle" type="text" class="mt-1 block w-full"
+                                        placeholder="Course Title" v-model="form.courseTitle" />
+                                </div>
+
+                                <!-- Course Instructor Input -->
+                                <div>
+                                    <InputLabel for="courseInstructor" value="Course Instructor" />
+                                    <TextInput id="courseInstructor" type="text" class="mt-1 block w-full"
+                                        placeholder="Course Instructor" v-model="form.courseInstructor" />
+                                </div>
+
+
+                            </div>
+
+                            <div class="flex justify-start gap-x-8 gap-y-8">
+                                <!-- Course Day Input -->
+                                <div>
+                                    <InputLabel for="courseDay" value="Course Day" />
+                                    <TextInput id="courseDay" type="text" class="mt-1 block w-full"
+                                        placeholder="Course Day" v-model="form.courseDay" />
+                                </div>
+                                <div>
+                                    <InputLabel for="courseDay" value="Course Day" />
+                                    <select id="courseDay" type="text"
+                                        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                        v-model="form.courseDay">
+
+                                        <option value="1">Monday</option>
+                                        <option value="2">Tuesday</option>
+                                        <option value="3">Wednesday</option>
+                                        <option value="4">Thursday</option>
+                                        <option value="5">Friday</option>
+                                        <option value="6">Saturday</option>
+                                        <option value="7">Sunday</option>
+                                    </select>
+                                </div>
+
+                                
+                            </div>
+                            <div class="flex justify-start gap-x-8 gap-y-8">
+                                <!-- Course Period Input -->
+                                <div>
+                                    <InputLabel for="coursePeriod" value="Course Period" />
+                                    <TextInput id="coursePeriod" type="text" class="mt-1 block w-full"
+                                        placeholder="Course Period" v-model="form.coursePeriod" />
+                                </div>
+                                <div>
+                                    <InputLabel for="coursePeriod" value="Course Period" />
+                                    <select id="coursePeriod" type="text"
+                                        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                        v-model="form.coursePeriod">
+
+                                        <option value="1">1 (08:10-09:00)</option>
+                                        <option value="2">2 (09:10-10:00)</option>
+                                        <option value="3">3 (10:10-11:00)</option>
+                                        <option value="4">4 (11:10-12:00)</option>
+                                        <option value="5">5 (12:10-13:00)</option>
+                                        <option value="6">6 (13:10-14:00)</option>
+                                        <option value="7">7 (14:10-15:00)</option>
+                                        <option value="8">8 (15:10-16:00)</option>
+                                        <option value="9">9 (16:10-17:00)</option>
+                                        <option value="10">10 (17:10-18:00)</option>
+                                        <option value="11">11 (18:30-19:20)</option>
+                                        <option value="12">12 (19:25-20:15)</option>
+                                        <option value="13">13 (20:25-21:15)</option>
+                                        <option value="14">14 (21:20-22:10)</option>
+                                    </select>
+                                </div>
+
+
+                            </div>
                         </div>
-
                     </div>
                 </div>
             </div>
         </form>
+
     </AuthenticatedLayout>
 </template>
